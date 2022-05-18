0f4f6edb78fa7ff172c729ea733dc6547eaf844f
Serjml <Serjml@list.ru>
2022-05-18 18:49:34 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.rm(path.join(__dirname, 'files-copy'), {recursive: true, force: true}, () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, () => {});
+  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, data) => {
+    data.forEach(el => {
+      if (el.isFile()) {
+        fs.copyFile(path.join(__dirname, 'files', el.name), path.join(__dirname, 'files-copy', el.name), () => {});
+      }
+    })
+  });
+})
+
+
+
+
 
