c60ba36dfa58e537d5ff37d6dabe4ed57050bede
Serjml <Serjml@list.ru>
2022-05-18 19:30:33 +0300

    fix: fix 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,14 +3,20 @@ const path = require('path');
 
 const folder = path.join(__dirname, 'styles');
 
-fs.readdir(folder, {withFileTypes: true}, (err, data) => {
-  data.forEach(el => {
-    if ((el.isFile) && (path.extname(el.name) === '.css')) {
-      console.log(el)
-      const readStream = fs.createReadStream(path.join(__dirname, 'styles', el.name), 'utf-8');      
-      readStream.on('data', (chunk) => {
-        fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), (chunk + '\n') , () => {});
-      }) 
-    }
+fs.rm(path.join(__dirname, 'project-dist', 'bundle.css'), {recursive: true, force: true}, () => {
+  fs.readdir(folder, {withFileTypes: true}, (err, data) => {
+    data.forEach(el => {
+      if ((el.isFile) && (path.extname(el.name) === '.css')) {
+        console.log(el)
+        const readStream = fs.createReadStream(path.join(__dirname, 'styles', el.name), 'utf-8');      
+        readStream.on('data', (chunk) => {
+          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), (chunk + '\n') , () => {});
+        }) 
+      }
+    })
   })
 })
+
+
+
+
